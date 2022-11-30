20d3e76e0662e13ffe0b31292af08e0bbdd27350
Daniel Alexander Fernandez <fdalexander1111@gmail.com>
2022-11-29 22:16:18 -0300

    se agrega las mejoras en la arquitectura de la api

---
+++ desafio_mejorar_la_arquitectura_de_nuestra_api/src/models/DTO/productDTO.js
@@ -0,0 +1,12 @@
+class productDTO{
+
+    constructor(data){
+
+        this.title = data.title;
+        this.price = data.price;
+        this.thumbnail = data.thumbnail;
+
+    }
+}
+
+export default productDTO;
---
+++ desafio_mejorar_la_arquitectura_de_nuestra_api/src/routes/products.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import { auth } from '../middlewares/auth.js';
+import ProductController from '../controller/ProductController.js';
+
+const router = Router();
+
+class RouterProduct{
+
+    constructor(){
+
+        this.productController = new ProductController();
+    }
+
+    start(){
+       
+        router.get("/", this.productController.getAll);
+        router.post("/", auth, this.productController.save);
+
+        return router;
+    }
+}
+
+
+export default RouterProduct;
