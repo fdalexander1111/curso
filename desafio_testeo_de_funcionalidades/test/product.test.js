acbd8a6591e97d56b27d60c64a15cc99fa0d357f
Daniel Alexander Fernandez <fdalexander1111@gmail.com>
2022-12-14 21:55:51 -0300

    se agrega edit y delete a las pruebas unitarias

--- desafio_testeo_de_funcionalidades/test/product.test.js
+++ desafio_testeo_de_funcionalidades/test/product.test.js
@@ -8,21 +8,53 @@ const agent = supertest("http://localhost:8080");
 describe('berificamos las apis de productos', function () {
 
     let product;
+    let productEdit;
+    let productDelete;
+
     beforeEach(()=> {
-      product = {
-        title: "Producto desde test",
-        price: 1010,
-        thumbnail: "foto test"
-      }
+        product = {
+            title: "Producto desde test",
+            price: 1010,
+            thumbnail: "foto test"
+        }
+        
+        productEdit = {
+            id: "63987d5d4ad609d82b083502",
+            title: "Producto edit desde test",
+            price: 1111,
+            thumbnail: "foto test edit"
+        }
+
+        productDelete = {
+            id: "639a70584ad609d82b083521"
+        }
     });
     it('deberia devolver un estatus 200 y salvar un producto', async function () {
         const response = await agent.post("/api/productos/")
         .send(product);
         const body = response.body;
-        console.log(body)
         expect(response.status).to.eql(200);
         expect(body).to.include.keys('status', 'message', 'code','result');
         expect(body.result.title).to.eql(product.title);
+    });
+
+    it('deberia devolver un estatus 200 y editar un producto', async function () {
+        console.log(product);
+        const response = await agent.put(`/api/productos/${productEdit.id}`)
+        .send(productEdit);
+        const body = response.body;
+        expect(response.status).to.eql(200);
+        expect(body).to.include.keys('status', 'message', 'code','product');
+        expect(body.product.title).to.eql(productEdit.title);
+    });
+
+    it('deberia devolver un estatus 200 y eliminar un producto', async function () {
+        console.log(product);
+        const response = await agent.delete(`/api/productos/${productDelete.id}`)
+        .send(productDelete);
+        const body = response.body;
+        expect(response.status).to.eql(200);
+        expect(body).to.include.keys('status', 'message', 'code');
     });
 });
 
